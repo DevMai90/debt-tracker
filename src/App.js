9b5d5f0defb968f8a1f4fcacfc573be9e6f28254
DevMai90 <dmai90@yahoo.com>
2019-02-26 00:16:06 -0800

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layout/AppNavbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>hihihihihi</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1 className="display-4">HELLLLLLLLO WORLD</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
