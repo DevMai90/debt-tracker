551216aca1bb2dc8630f91ae9a8442b11cb17680
DevMai90 <dmai90@yahoo.com>
2019-03-03 12:42:24 -0800

    Completed update balance and delete functions

--- src/components/debtors/DebtDetails.js
+++ src/components/debtors/DebtDetails.js
@@ -8,8 +8,115 @@ import { Spinner } from '../layout/Spinner';
 import classnames from 'classnames';
 
 class DebtDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    showMonthlyPmtUpdate: false,
+    balanceUpdateAmount: '',
+    monthlyPmtUpdateAmount: ''
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onDeleteClick = () => {
+    const { firestore, debtor, history } = this.props;
+
+    firestore
+      .delete({ collection: 'debtors', doc: debtor.id })
+      .then(history.push('/'));
+  };
+
+  balanceSubmit = e => {
+    e.preventDefault();
+
+    const { firestore, debtor } = this.props;
+    const { balanceUpdateAmount } = this.state;
+
+    // Create object with info to be updated
+    const updatedBalance = {
+      balance: balanceUpdateAmount
+    };
+
+    // firestore has .update() because of firestoreConnect()
+    // 1st arg -> collection and document that we're working with. (object)
+    // 2nd arg -> what we're updating
+    firestore.update({ collection: 'debtors', doc: debtor.id }, updatedBalance);
+  };
+
+  monthlyPmtSubmit = e => {
+    e.preventDefault();
+
+    const { firestore, debtor } = this.props;
+    const { monthlyPmtUpdateAmount } = this.state;
+
+    const updatedPmtAmount = {
+      monthlyPmt: monthlyPmtUpdateAmount
+    };
+
+    firestore.update(
+      { collection: 'debtors', doc: debtor.id },
+      updatedPmtAmount
+    );
+  };
+
   render() {
     const { debtor } = this.props;
+    const {
+      showBalanceUpdate,
+      showMonthlyPmtUpdate,
+      balanceUpdateAmount,
+      monthlyPmtUpdateAmount
+    } = this.state;
+
+    // Update Monthly Payment
+    let monthlyPmtForm;
+
+    if (showMonthlyPmtUpdate) {
+      monthlyPmtForm = (
+        <form onSubmit={this.monthlyPmtSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              name="monthlyPmtUpdateAmount"
+              placeholder="New Monthly Payment"
+              onChange={this.onChange}
+              value={monthlyPmtUpdateAmount}
+            />
+            <div className="input-group-append">
+              <input type="submit" className="btn btn-outline-dark" />
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      monthlyPmtForm = null;
+    }
+
+    // Update Outstanding Balance
+    let balanceForm;
+
+    if (showBalanceUpdate) {
+      balanceForm = (
+        <form onSubmit={this.balanceSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              name="balanceUpdateAmount"
+              placeholder="New Outstanding Balance"
+              onChange={this.onChange}
+              value={balanceUpdateAmount}
+            />
+            <div className="input-group-append">
+              <input type="submit" className="btn btn-outline-dark" />
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      balanceForm = null;
+    }
+
     if (debtor) {
       return (
         <React.Fragment>
@@ -23,9 +130,11 @@ class DebtDetails extends Component {
             <div className="col-md-6">
               <div className="btn-group float-right">
                 <Link to={`/debtor/edit/${debtor.id}`} className="btn btn-dark">
-                  Edit
+                  <i className="far fa-edit" /> Edit
                 </Link>
-                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-danger" onClick={this.onDeleteClick}>
+                  <i className="far fa-trash-alt" /> Delete
+                </button>
               </div>
             </div>
           </div>
@@ -37,17 +146,47 @@ class DebtDetails extends Component {
               <h4>{debtor.debtorName}</h4>
             </div>
             <div className="card-body">
-              <div className="row">
-                <div className="col-md-8 col-sm-6">
+              <div className="d-flex justify-content-start row-hl">
+                <div className="item-hl">
                   <h5>
                     Debtor ID:{' '}
                     <span className="text-secondary">{debtor.id}</span>
                   </h5>
                 </div>
+              </div>
 
-                <div className="col-md-4 col-sm-6">
-                  <h5 className="float-right">
-                    Balance:{' '}
+              <div className="d-flex justify-content-between row-hl">
+                <div className="item-hl">
+                  <h5>
+                    Monthly Payment:{' '}
+                    <span
+                      className={classnames({
+                        'text-danger': debtor.monthlyPmt > 0,
+                        'text-success': debtor.monthlyPmt == 0
+                      })}
+                    >
+                      ${parseFloat(debtor.monthlyPmt).toFixed(2)}
+                    </span>{' '}
+                    <small>
+                      <a
+                        href="#!"
+                        onClick={() =>
+                          this.setState({
+                            showMonthlyPmtUpdate: !this.state
+                              .showMonthlyPmtUpdate
+                          })
+                        }
+                      >
+                        <i className="fas fa-pencil-alt" />
+                      </a>
+                    </small>
+                  </h5>
+                  {monthlyPmtForm}
+                </div>
+
+                <div className="item-hl">
+                  <h5>
+                    Outstanding Balance:{' '}
                     <span
                       className={classnames({
                         'text-danger': debtor.balance > 0,
@@ -55,8 +194,21 @@ class DebtDetails extends Component {
                       })}
                     >
                       ${parseFloat(debtor.balance).toFixed(2)}
-                    </span>
+                    </span>{' '}
+                    <small>
+                      <a
+                        href="#!"
+                        onClick={() =>
+                          this.setState({
+                            showBalanceUpdate: !this.state.showBalanceUpdate
+                          })
+                        }
+                      >
+                        <i className="fas fa-pencil-alt" />
+                      </a>
+                    </small>
                   </h5>
+                  {balanceForm}
                 </div>
               </div>
 
